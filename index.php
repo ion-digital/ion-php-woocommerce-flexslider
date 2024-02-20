8b424c21c82e105cc92a531664dda9ecabfa9ebf
jmeyer-vlsystems <92323513+jmeyer-vlsystems@users.noreply.github.com>
2024-02-20 13:32:49 +0200

    Package update

--- index.php
+++ index.php
@@ -1,2 +1,16 @@
 <?php
 
+/* 
+ * See license information at the package root in LICENSE.md
+ */
+
+$bootstrap = realpath(__DIR__ . "/vendor/ion/packaging/bootstrap.php") ?: realpath(__DIR__ . "/../packaging/bootstrap.php");
+
+if(!empty($bootstrap))
+    require_once($bootstrap);
+
+\Ion\Package::create("ion", "woocommerce-flexslider", function($package) {
+
+    return;
+
+}, __FILE__);
